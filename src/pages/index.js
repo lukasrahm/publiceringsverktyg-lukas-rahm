01f4b2998ee8c7df6f0dd9fcf608b7cb9855e5b8
lukasrahm <55479438+lukasrahm@users.noreply.github.com>
2020-10-07 17:19:40 +0200

    blog update

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,18 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
-export default ({data}) => {
-  const {allStrapiProjects:{nodes:projects},
-} = data
+export default ({ data }) => {
+  const {
+    allStrapiProjects: { nodes: projects },
+    allStrapiBlogs: { nodes: blogs },
+  } = data
 
   return <Layout>
     <Hero/>
     <Services/>
     <Jobs/>
     <Projects projects={projects} title="featured projects" showLink/>
+    <Blogs blogs={blogs} title="latest articles" showLink />
   </Layout>
 }
 
@@ -23,15 +26,11 @@ export const query = graphql`
   {
     allStrapiProjects(filter: {featured: {eq: true}}, sort: {fields: strapiId, order: DESC}) {
       nodes {
-        stack {
-          id
-          title
-        }
-        title
-        url
         github
         id
         description
+        title
+        url
         image {
           childImageSharp {
             fluid {
@@ -39,14 +38,18 @@ export const query = graphql`
             }
           }
         }
+        stack {
+          id
+          title
+        }
       }
     }
-    allStrapiBlogs(sort: {fields: date, order: DESC}, limit: 3) {
+    allStrapiBlogs(sort: { fields: date, order: DESC }, limit: 3) {
       nodes {
         slug
         content
         desc
-        date(formatString: "MMM Do, YYYY")
+        date(formatString: "MMMM Do, YYYY")
         id
         title
         category
